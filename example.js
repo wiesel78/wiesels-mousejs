3454553c2e913440f67072aca10c452d57c66847
Philip Ullrich <wurst78@gmail.com>
2014-07-05 16:08:18 +0200

    examples

---
+++ example.js
@@ -0,0 +1,19 @@
+var mouse = require("./index.js");
+
+
+// get and set the y position
+console.log("y : ", mouse.y());
+console.log("set the y position to 100 ");
+mouse.y( 100 );
+
+// get and set the x position
+console.log("x : ", mouse.x());
+console.log("set the x position to 100 ");
+mouse.x( 100 );
+
+// move the mouse position to x=200 y=200
+console.log("move to x=200, y=200");
+mouse.move( 200, 200 );
+
+mouse.close();
+
--- start.js
+++
@@ -1,9 +0,0 @@
-var mouse = require("./index.js");
-
-console.log("y : ", mouse.y());
-mouse.y(100);
-
-console.log("x : ", mouse.x());
-mouse.x(350);
-mouse.close();
-
