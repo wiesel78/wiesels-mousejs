ec8430b4784c2e07c286a2a8386b2ec62bc744c2
Philip Ullrich <wurst78@gmail.com>
2015-03-15 11:20:25 +0100

    fix ffi bug on windows - replace ffi with edgejs

--- win32/mouse.js
+++ win32/mouse.js
@@ -1,25 +1,105 @@
+var edge = require("edge");
 
-var ffi = require("ffi");
-var ref = require("ref");
-var struct = require("ref-struct");
+/*
+   Using EdgeJS as alternative of node-ffi
+*/
+var user32 = {
+    GetCursorPos: edge.func(function () {/*
+        using System.Threading.Tasks;
+        using System.Runtime.InteropServices;
 
-var pointStr = struct({
-	'x' : 'long',
-	'y' : 'long'
-});
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
 
-var pointStrPtr = ref.refType(pointStr);
+        public class Startup {
+            public async Task<object> Invoke(object input){
+                return Mouse.GetCursorPosition();
+            }
+        }
 
-var user32 = ffi.Library('user32', {
-	'SetCursorPos' : ['long', ['long', 'long']],
-	'GetCursorPos' : ['bool', [pointStrPtr]]
-});
+        public static class Mouse {
+            [DllImport("user32.dll")]
+            public static extern bool GetCursorPos(out POINT lpPoint);
 
+            public static POINT GetCursorPosition()
+            {
+                POINT lpPoint;
+                GetCursorPos(out lpPoint);
+                return lpPoint;
+            }
+        }
+    */
+    }),
+    SetCursorPos: edge.func(function () {/*
+        using System.Threading.Tasks;
+        using System.Runtime.InteropServices;
+        
+        public struct POINT
+        {
+            public int x;
+            public int y;
+        }
 
-var point = new pointStr();
+        public class Startup {
+            public async Task<object> Invoke(dynamic input){
+                int x = (int) input.x;
+                int y = (int) input.y;
+                return Mouse.SetCursorPosition(x, y);
+            }
+        }
+
+        public static class Mouse {
+            [DllImport("user32.dll")]
+            public static extern bool SetCursorPos(int x, int y);
+
+            public static bool SetCursorPosition(int y, int x)
+            {
+                return SetCursorPos(x, y);
+            }
+        }
+    */
+    }),
+    MouseEvent: edge.func(function () {/*
+        using System.Threading.Tasks;
+        using System.Runtime.InteropServices;
+
+        public class Startup {
+            public async Task<object> Invoke(dynamic input){
+                uint flags = (uint) input.flags;
+
+                return Mouse.MouseEvent(flags);
+            }
+        }
+
+        public static class Mouse {
+            [DllImport("user32.dll")]
+            public static extern void mouse_event(uint flags, uint x, uint y, uint btn, uint info);
+
+            public static bool MouseEvent(uint flags)
+            {
+                mouse_event( flags, 0, 0, 0, 0 );
+                return true;
+            }
+        }
+    */
+    })
+};
+
+/* 
+   Values of mouse events
+*/
+var MOUSEEVENT = {
+    LEFTDOWN    : 0x02,
+    LEFTUP      : 0x04,
+    RIGHTDOWN   : 0x08,
+    RIGHTUP     : 0x10
+};
 
 module.exports.move = function( x, y){
-	user32.SetCursorPos( x, y );
+    user32.SetCursorPos({ x: x, y: y });
 }
 
 module.exports.moveRelative = function( x, y ){
@@ -31,17 +111,7 @@ module.exports.moveRelative = function( x, y ){
 }
 
 module.exports.get = function(){
-	if( user32.GetCursorPos(point.ref()) ){
-		return {
-			x : point.x,
-			y : point.y
-		};
-	}else{
-		return {
-			x : 0,
-			y : 0
-		}
-	}
+    return user32.GetCursorPos(null, true); 
 }
 
 module.exports.x = function( x ){
@@ -61,5 +131,5 @@ module.exports.y = function( y ){
 }
 
 module.exports.click = function(){
-
+    user32.MouseEvent({flags : MOUSEEVENT.LEFTDOWN | MOUSEEVENT.LEFTUP}, true);
 }
